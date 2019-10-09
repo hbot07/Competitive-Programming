f033addd75e2182ad2c4f6505db8e8db1a888987
Parth <hbot07@gmail.com>
2019-10-09 14:57:37 +0530

    codechef

--- src/TEST.java
+++ src/TEST.java
@@ -3,9 +3,10 @@ public class TEST {
 public static void main(String args[])
 {
     Scanner sc=new Scanner(System.in);
-    long n;
-    do{
+    long n=sc.nextLong();
+    do{ System.out.println(n);
         n=sc.nextLong();
+
     }while(n!=42);
 }
 
