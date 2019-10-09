ac070e2186ba59e10019294bacbcbf80b2c2324f
Parth <hbot07@gmail.com>
2019-10-09 14:54:31 +0530

    codechef

---
+++ src/TEST.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TEST {
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    long n;
+    do{
+        n=sc.nextLong();
+    }while(n!=42);
+}
+
+}
