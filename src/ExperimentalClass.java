9ca6e297361db9eff59775ed112ef3c206e0788c
Parth <hbot07@gmail.com>
2019-10-30 11:06:16 +0530

    IYMC Pre Final Problem C1 .

--- src/ExperimentalClass.java
+++ src/ExperimentalClass.java
@@ -1,6 +1,13 @@
 public class ExperimentalClass {
-   // public static void main(String[] args)
-    //{
-      //  int[] arr= alloc_array(int,10);
-    //}
+   public static void main(String[] args)
+    {
+    System.out.println("Execution Started");
+
+   for(int i=0;i<=10;i++)
+   {
+       System.out.println((Math.pow(7,i+1)-1)/(Math.pow(7,i)*(7-1)));
+   }
+
+
+    }
 }
---
+++ src/IYMCPreFinalC1.java
@@ -0,0 +1,26 @@
+public class IYMCPreFinalC1 {
+
+public static void main(String[] args)
+{double sum=0;
+for(int j=0;j<=10;j++)
+     for(int i=0;i<=10;i++)
+    for(int i1=0;i1<=10;i1++)
+    {
+        for(int i2=0;i2<10;i2++)
+        {
+            for(int i3=0;i3<=10;i3++)
+            {
+               sum= ((Math.pow(2,i1+1)-1)/(2-1))*((Math.pow(3,i2+1)-1)/(3-1))*((Math.pow(5,i3+1)-1)/(5-1))*((Math.pow(7,i+1)-1)/(7-1))*((Math.pow(11,j+1)-1)/(11-1));
+               //if(Math.pow(2,i1)*Math.pow(3,i2)*Math.pow(5,i3)==240) System.out.println(sum);
+               if(sum>=4*Math.pow(2,i1)*Math.pow(3,i2)*Math.pow(5,i3)*Math.pow(7,i)*Math.pow(11,j)&&Math.pow(2,i1)*Math.pow(3,i2)*Math.pow(5,i3)*Math.pow(7,i)*Math.pow(11,j)<2000)
+                    System.out.println(Math.pow(2,i1)*Math.pow(3,i2)*Math.pow(5,i3)*Math.pow(7,i)*Math.pow(11,j));
+
+            //if(Math.pow(2,i1)*Math.pow(3,i2)*Math.pow(5,i3)*Math.pow(7,i)*Math.pow(11,j)==11) {System.out.println(sum);break;}
+            }
+
+        }
+
+    }
+}
+
+}
