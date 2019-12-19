3bab1e978c1b21c57535a53eda895ce27ee4c79d
Parth <hbot07@gmail.com>
2019-12-19 19:39:09 +0530

    efficient Dynamic Programming solution of "Traditional Rod Cutting Problem" modifying code from RodCutting.java

---
+++ src/RodCuttingDP.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class RodCuttingDP {
+    static int[] a,memo;
+    static boolean[] isAlreadyCalculated;
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String args[])
+    {
+
+        int n=sc.nextInt();
+        a=new int[n];memo=new int[n];
+        isAlreadyCalculated=new boolean[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+
+        }
+
+        System.out.println(r(n));
+
+    }
+    static int r(int n)
+    {if(n==1) return a[0];
+        if(n==0) return 0;
+        if (isAlreadyCalculated[n-1]) return memo[n-1];
+        int max=a[n-1];//the revenue when no cut would be made
+        for(int i=1;i<n;i++)
+        {
+            max=Math.max(r(i)+r(n-i),max);
+        }
+        memo[n-1]=max;isAlreadyCalculated[n-1]=true;
+        return max;
+    }
+}
