5b5c85c4037350e42fccae967bc81494697707ee
Parth <hbot07@gmail.com>
2020-03-04 21:21:18 +0530

    improved code readability

--- src/MyFirstGraph.java
+++ src/MyFirstGraph.java
@@ -6,23 +6,23 @@ public class MyFirstGraph {
     private static ArrayList<Integer>[] adjacencyList;
     public static void main(String[] args)
     {
-        System.out.println("This program prints all vertices connected to a given vertex");
+        System.out.println("This program prints all vertices connected to a given vertex in an undirected graph.");
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter no. of vertices");
+        System.out.println("Enter number of vertices");
         numberOfVertices=sc.nextInt();
         adjacencyList=new ArrayList[numberOfVertices];
         for(int i=0;i<numberOfVertices;i++)
             adjacencyList[i]=new ArrayList<>(numberOfVertices);
-        System.out.println("Enter no. of edges");
+        System.out.println("Enter number of edges");
         int numberOfEdges=sc.nextInt();
         for(int i=0;i<numberOfEdges;i++)
         {   
-            System.out.println("Enter pair of connected vertices");
+            System.out.println("Enter a pair of connected vertices");
             int vertex1=sc.nextInt(),vertex2=sc.nextInt();
             adjacencyList[vertex1].add(vertex2);
             adjacencyList[vertex2].add(vertex1);
         }
-        System.out.println("You want to find connected vertices to which vertex?");
+        System.out.println("Find connected vertices to which vertex?(Enter vertex number)");
         for(Integer i:allConnectedTo(sc.nextInt()))
         {
             System.out.println(i);
