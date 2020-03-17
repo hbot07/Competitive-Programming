84fdd30735ea0ccd84a64e237ccc8218c28d6735
Parth <hbot07@gmail.com>
2020-03-17 11:16:09 +0530

    https://www.codechef.com/IARCSJUD/problems/VOTERSDI. AC

--- src/VOTERSDI.java
+++ src/VOTERSDI.java
@@ -1,37 +1,25 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class VOTERSDI {
+public class VOTERSDI{
     static Scanner sc=new Scanner(System.in);
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        int n1=sc.nextInt(),n2=sc.nextInt(),n3=sc.nextInt();
-        ArrayList<Integer> a1=new ArrayList<>(n1),a2=new ArrayList<>(n2),a3=new ArrayList<>(n3);
-        for(int i=0;i<n1;i++)
-            a1.add(sc.nextInt());
-        for(int i=0;i<n2;i++)
-            a2.add(sc.nextInt());
-        for(int i=0;i<n3;i++)
-            a3.add(sc.nextInt());
-        ArrayList<Integer> a=new ArrayList<>();
-        for(int i=0;i<n1;i++)
+        int n=sc.nextInt()+sc.nextInt()+sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        Arrays.sort(a);
+        ArrayList<Integer> ar=new ArrayList<>();
+        int freq=0,element=-1;
+        for(int i=0;i<n-1;i++)
         {
-            if(a2.contains(a1.get(i)) || a3.contains(a1.get(i)))
-            {
-                a.add(a1.get(i));
-                a2.remove(a1.get(i));
-            }
+            if(a[i]==a[i+1] && !ar.contains(a[i]))
+                ar.add(a[i]);
         }
-        for(int i=0;i<a2.size();i++){
-            if(a3.contains(a2.get(i)))
-            {
-                a.add(a2.get(i));
-            }
-        }
-        Collections.sort(a);
-        System.out.println(a.size());
-        for(int i=0;i<a.size();i++)
-            System.out.println(a.get(i));
+        System.out.println(ar.size());
+        for(int i=0;i<ar.size();i++)
+            System.out.println(ar.get(i));
     }
 }
